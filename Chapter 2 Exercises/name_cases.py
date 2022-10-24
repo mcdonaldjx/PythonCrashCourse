390b62299a4b432547372b7c6376b57c99802afb
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-10-24 15:56:49 -0500

    Create name_cases.py
    Chapter 2, Exercise 4

---
+++ Chapter 2 Exercises/name_cases.py
@@ -0,0 +1,4 @@
+#Created 10/24/2022
+#Chapter 2, Exercise 4- Name Cases: Use a variable to represent a person's name, and then print that person's name in lowercase, uppercase, and title case.
+name = input("What is your name?")
+print(f"Here is your name in...\nLowercase: {name.lower()}\nUppercase: {name.upper()}\nTitle Case: {name.title()}")
