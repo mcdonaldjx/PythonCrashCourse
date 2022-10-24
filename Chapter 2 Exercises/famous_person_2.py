6bba0332ad08111946a7534ab2d4690b835bfcca
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-10-24 16:16:17 -0500

    Create famous_person_2.py
    Chapter 2, Exercise 6

---
+++ Chapter 2 Exercises/famous_person_2.py
@@ -0,0 +1,6 @@
+#Created 10/24/2022
+#Chapter 2, Exercise 6- Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person's name using a variable called_famous person. Then compose your message and represent it with a new variable called message. Print your message.
+famous_person = "Albert Einstein"
+quote = "A person who never made a mistake never tried anything new."
+message = f"{famous_person} once said, \"{quote}\""
+print(message)
