cf7e5c5a0037afb12958ff88245611711f0b66ef
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-10-24 15:52:49 -0500

    Create personal_message.py
    Chapter 2, Exercise 3

---
+++ Chapter 2 Exercises/personal_message.py
@@ -0,0 +1,4 @@
+#Created 10/24/2022 by Jared
+#Chapter 2, Exercise 3- Personal Message: Use a variable to represent a person's name, and print a message to that person. Your message should be simple, such as, "Hello Eric, would you like to larn some Python today?"
+name = input("What is your name?")
+print(f"Hello, {name}! I'm personal_message.py")
