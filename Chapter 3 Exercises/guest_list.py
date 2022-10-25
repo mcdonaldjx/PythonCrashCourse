5a50d279815159a8cf37d81ee5bd4c691845bcf7
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-10-25 18:40:06 -0500

    Create guest_list.py
    Exercise 3-4

---
+++ Chapter 3 Exercises/guest_list.py
@@ -0,0 +1,10 @@
+#Created 10/25/2022 by Jared
+# Exercise 3-4 - Guest List: If you could invite anyone, living or deceased to dinner, who would you invite? Make a list that includes at least three people you'd like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+invites = []
+invites.append("Andre Benjamin")
+invites.append("Barack Obama")
+invites.append("Denzel Washington")
+invites.append("Jamie Foxx")
+for i in invites:
+  print(f"Join me for dinner please, {i}")
+  
