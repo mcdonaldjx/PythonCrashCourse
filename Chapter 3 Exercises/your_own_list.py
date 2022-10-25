0d6054e2bc9c56823dc42f493f17ce4555620895
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-10-25 18:24:23 -0500

    Create your_own_list.py
    Exercise 3-3

---
+++ Chapter 3 Exercises/your_own_list.py
@@ -0,0 +1,5 @@
+#Created 10/25/2022 by Jared
+# Exercise 3-3 - Your Own List: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores serveral examples. Use your list to print a series of statements about these items, such as "I would like to own a Honda motorcycle."
+cars = ["Viper", "Camry", "Cadillac", "Camaro"]
+for c in cars:
+  print(f"I'd like to drive a {c} someday")
