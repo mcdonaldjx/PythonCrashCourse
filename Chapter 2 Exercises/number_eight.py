860f0b1ceaad25a04526102b089a351426c50ace
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-10-24 20:42:06 -0500

    Create number_eight.py
    Exercise 2-9

---
+++ Chapter 2 Exercises/number_eight.py
@@ -0,0 +1,10 @@
+##########
+#Created 10/24/2022 by Jared
+##Exercise 2-9: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print() calls to see the results. You should create four lines that look like this:
+##print(5+3)
+##Your output should be four lines, with the number 8 appearing once on each line.
+##########
+print(2+6)
+print(10-2)
+print(4*2)
+print(24/3)
