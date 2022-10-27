006eb32a1eade6b6466fc64a8f6283e959c7c38f
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-10-26 20:07:01 -0500

    Create cubes.py
    Exercise 4-8

---
+++ Chapter 4 Exercises/cubes.py
@@ -0,0 +1,5 @@
+#Created 10/26/2022
+#Exercise 4-8 - Cubes: A number raised to the third power is called a cube. For example, the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10), and use a for loop to print out the value of each cube.
+cubes = [x**3 for x in range(1,11)]
+for cube in cubes:
+  print(cube)
