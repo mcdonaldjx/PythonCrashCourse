41c8a443c481dbb8158bad90c35fe19ebcf9c07a
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-10-26 20:03:49 -0500

    Create threes.py
    Exercise 4-7

---
+++ Chapter 4 Exercises/threes.py
@@ -0,0 +1,5 @@
+#Created 10/26/2022
+#Exercise 4-7 - Threes: Make a list of the multiples of 3, from 3 to 30. Use a for loop to print the numbers in your list.
+threes = list(range(3,30,3))
+for t in threes:
+  print(t)
