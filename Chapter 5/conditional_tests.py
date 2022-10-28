49d7723e6a7fa781d08cb3358215c52776ca8244
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-10-27 19:47:41 -0500

    Create conditional_tests.py
    Exercise 5-1

---
+++ Chapter 5/conditional_tests.py
@@ -0,0 +1,23 @@
+#Created 10/27/2022
+#Exercise 5-11- Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.
+#  Look closely at your results, and make sure you understand why each line evaluates to True or False.
+#  Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+cars = ["audi", "toyota","kia","ford","nissan","honda"]
+print("Is toyota in the list? I predict True")
+print("toyota" in cars)
+print("\nIs GT in the list? I predict False")
+print("GT" in cars)
+print("\nIs Ford in the list? I predict True")
+print("ford" in cars)
+print("\nIs Acura in the list? I predict False")
+print("toyota" in cars)
+print("\nIs nissan in the list? I predict True")
+print("nissan" in cars)
+print("\nIs audi in the list? I predict True")
+print("audi" in cars)
+print("\nIs bmw in the list? I predict False")
+print("bmw" in cars)
+print("\nIs buick in the list? I predict False")
+print("buick" in cars)
+print("\nIs kia in the list? I predict True")
+print("kia" in cars)
