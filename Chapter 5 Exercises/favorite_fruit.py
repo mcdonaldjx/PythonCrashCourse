a6e45b16ef494f94a4ac70a20ad136cd9c5448cf
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-10-28 20:39:04 -0500

    Create favorite_fruit.py
    Exercise 5-7

---
+++ Chapter 5 Exercises/favorite_fruit.py
@@ -0,0 +1,15 @@
+#Created 10/28/2022
+#Exercise 5-7- Favorite Fruit: Make a list of your favorite fruits, and then write a series of independent if statement that check for certain fruits in your list.
+#  Make a list of your three favorite fruits and call it favorite_fruits
+#  Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block should print a statement, such as You really like bananas!
+favorite_fruits = ["pears", "apples", "grapes"]
+if "pears" in favorite_fruits:
+  print("You really like pears!")
+if "pineapples" in favorite_fruits:
+  print("You really like pineapples!")
+if "apples" in favorite_fruits:
+  print("You really like apples!")
+if "grapes" in favorite_fruits:
+  print("You really like grapes!")
+if "strawberries" in favorite_fruits:
+  print("You really like strawberries!")
