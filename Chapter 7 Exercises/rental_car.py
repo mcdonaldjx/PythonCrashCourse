f2cf1f2b11b9436022c90e6c04bc8c8666bd2adb
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-11-02 20:33:19 -0500

    Create rental_car.py

---
+++ Chapter 7 Exercises/rental_car.py
@@ -0,0 +1,4 @@
+#Created 11/2/2022
+#Exercise 7-1- Rental Car: Write a program that asks the user what kind of rental car they would like. Print a message about that car, such as "Let me see if I c an find you a Subaru."
+desired_car = input("What kind of car would you like? ")
+print(f"I'll see if there's a {desired_car} in stock.")
