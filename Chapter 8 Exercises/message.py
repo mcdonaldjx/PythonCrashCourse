8c5482ea6c481fdb73d3de9765afbcd1361074b8
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-11-10 19:47:07 -0600

    Create message.py

---
+++ Chapter 8 Exercises/message.py
@@ -0,0 +1,7 @@
+#Created 11/10/2022 by Jared
+#Exercise 8.1- Message: Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. Call the function, and make sure the message displays correctly.
+def display_message():
+  """This method displays a message."""
+  print("In this chapter, we are learning about functions. How to make them, call them, etc.")
+
+display_message()
