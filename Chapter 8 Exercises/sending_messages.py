8489c29067ed70121966aa64353989e68042f523
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-11-25 05:24:50 -0600

    Update sending_messages.py

--- Chapter 8 Exercises/sending_messages.py
+++ Chapter 8 Exercises/sending_messages.py
@@ -2,13 +2,15 @@
 #Exercise 8-10- Sending Messages: Start with a copy of your program from Exercise 8-9. Write a function called send_messages() that prints each text message and moves each message to a new list called sent_messages as it's printed. After calling the function, print both of your lists to make sure the messages were moved correctly.
 def send_messages(text_messages):
   sent_messages = []
-  for text in text_messages:
-    sent_messages.append(text)
+  while text_messages:
+    sent_messages.append(text_messages.pop())
   return sent_messages
 text_messages = ["good morning", "hello", "good night", "see you soon"]
 sent_messages = send_messages(text_messages)
+print("Messages to send:")
 for text in text_messages:
-  print(f"Sending {text}...")
+  print(text)
 print()
+print("Messages sent:")
 for sent_text in sent_messages:
-  print(f"Sent {sent_text}!")
+  print(f"{sent_text}")
