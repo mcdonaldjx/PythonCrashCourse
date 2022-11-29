27fd1922f421f043bd8349f0c7dfb14ddbeb6bac
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-11-29 10:56:29 -0600

    Update printing_functions.py

--- Chapter 8 Exercises/Exercise 8-15/printing_functions.py
+++ Chapter 8 Exercises/Exercise 8-15/printing_functions.py
@@ -11,9 +11,3 @@ def show_completed_models(completed_models):
   print("\nThe following models have been printed:")
   for completed_model in completed_models:
     print(completed_model)
-  
-unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
-completed_models = []
-
-print_models(unprinted_designs, completed_models)
-show_completed_models(completed_models)
