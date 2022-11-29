a993e1c1cfbe4e6bb6f34300233715e97a7944f1
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-11-29 11:25:11 -0600

    Create import_4.py

---
+++ Chapter 8 Exercises/Exercise 8-16/import_4.py
@@ -0,0 +1,10 @@
+import album as alb
+#Created 11/29/2022 by Jared
+#Exercise 8-16- Imports: Using a program you wrote that has one function in it, store that function in a separate file. Import the function into your main program file, and call the function using each of these approaches:
+# import module_name
+# from module_name import function_name
+# from module_name import function_name as fn
+# import module_name as mn
+# from module_name import *
+music_album = alb.make_album("J.I.D.","DiCapario2,", num_of_songs=14)
+print(music_album)
