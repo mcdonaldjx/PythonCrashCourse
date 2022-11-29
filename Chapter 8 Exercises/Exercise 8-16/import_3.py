7a6085540bb87ec224d34747b61bae50eee4410f
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-11-29 11:34:27 -0600

    Update import_3.py

--- Chapter 8 Exercises/Exercise 8-16/import_3.py
+++ Chapter 8 Exercises/Exercise 8-16/import_3.py
@@ -3,7 +3,7 @@ from album import make_album as ma
 #Exercise 8-16- Imports: Using a program you wrote that has one function in it, store that function in a separate file. Import the function into your main program file, and call the function using each of these approaches:
 # import module_name
 # from module_name import function_name
-# from module_name import function_name as fn
+# from module_name import function_name as fn <---
 # import module_name as mn
 # from module_name import *
 music_album = ma("J.I.D.","DiCapario2,", num_of_songs=14)
