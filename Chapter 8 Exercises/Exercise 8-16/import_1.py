42790689bac8eb2c2fd21b1b99d2f5b009110e65
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-11-29 11:35:18 -0600

    Update import_1.py

--- Chapter 8 Exercises/Exercise 8-16/import_1.py
+++ Chapter 8 Exercises/Exercise 8-16/import_1.py
@@ -1,8 +1,7 @@
 #Created 11/29/2022 by Jared
 import album
-#Created 11/29/2022 by Jared
 #Exercise 8-16- Imports: Using a program you wrote that has one function in it, store that function in a separate file. Import the function into your main program file, and call the function using each of these approaches:
-# import module_name
+# import module_name <---
 # from module_name import function_name
 # from module_name import function_name as fn
 # import module_name as mn
