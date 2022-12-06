d5f9baf0e66eca40110a7da0166286e7599c3da8
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-12-05 18:54:47 -0600

    Create save_favorite_number.py

---
+++ Chapter 10 Exercises/Exercise 10-11/save_favorite_number.py
@@ -0,0 +1,13 @@
+#Created 12/5/2022 by Jared
+#Exercise 10-12- Favorite Number: Write a program that prompts for the user’s favorite number. Use json.dumps() to store this number in a file.
+from pathlib import Path
+import json
+path = Path('favorite_number.json')
+try:    
+  fave_number = int(input("Plese enter your favorite number so I can remember it: "))
+except ValueError:
+  print("That wasn't a number!")
+else:
+  print(f"Ok, I'll remember {fave_number}!")
+  save_data = json.dumps(fave_number)
+  path.write_text(save_data)
