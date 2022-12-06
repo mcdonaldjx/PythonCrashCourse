7ff329a91f5fa4c6d192ee315246204aef102e1c
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-12-06 09:44:58 -0600

    Update city_functions_b.py

--- Chapter 11 Exercises/Exercise 11-2/city_functions_b.py
+++ Chapter 11 Exercises/Exercise 11-2/city_functions_b.py
@@ -1,4 +1,4 @@
 #Created 12/06/2022
 #Exercise 11-2- Population: Modify the function so the population parameter is optional. Run the test, and make sure test_city_country() passes again.
-def get_city_country(city, country, population = 5000000):
+def get_city_country(city, country, population = ' '):
   return f"{city.title()}, {country.title()} - population {population}"
