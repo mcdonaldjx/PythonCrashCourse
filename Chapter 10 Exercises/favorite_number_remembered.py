05d95f33eb3d34f6bfb9fe666ea9830e1d0038a8
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-12-05 18:50:46 -0600

    Update and rename favorite_number.py to favorite_number_remembered.py

--- Chapter 10 Exercises/favorite_number_remembered.py
+++ Chapter 10 Exercises/favorite_number_remembered.py
@@ -1,5 +1,5 @@
 #Created 12/5/2022 by Jared
-#Exercise 10-10- Favorite Number: Write a program that prompts for the user’s favorite number. Use json.dumps() to store this number in a file. Write a separate program that reads in this value and prints the message “I know your favorite number! It’s _____.”
+#Exercise 10-12- Favorite Number Remembered: Combine the two programs you wrote in Exercise 10-11 into one file. If the number is already stored, report the favorite number to the user. If not, prompt for the user’s favorite number and store it in a file. Run the program twice to see that it works.
 from pathlib import Path
 import json
 
