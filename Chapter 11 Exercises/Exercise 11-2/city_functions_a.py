693321f3397e85f50e04cc947dd6c9e4d6476343
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-12-06 08:53:09 -0600

    Update and rename city_functions.py to city_functions_a.py

--- Chapter 11 Exercises/Exercise 11-2/city_functions_a.py
+++ Chapter 11 Exercises/Exercise 11-2/city_functions_a.py
@@ -1,4 +1,4 @@
 #Created 12/06/2022
 #Exercise 11-2- Population: Modify your function so it requires a third parameter, population. It should now return a single string of the form City, Country – population xxx, such as Santiago, Chile – population 5000000. Run the test again, and make sure test_city_country() fails this time.
-def get_city_country(city, country, population = 0):
+def get_city_country(city, country, population):
   return f"{city.title()}, {country.title()} - population {population}"
