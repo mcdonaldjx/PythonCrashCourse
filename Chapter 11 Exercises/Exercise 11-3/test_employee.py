72ce90431aa7b829c999fc4ee4ddd85344ac7e3a
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-12-07 07:17:21 -0600

    Create test_employee.py

---
+++ Chapter 11 Exercises/Exercise 11-3/test_employee.py
@@ -0,0 +1,18 @@
+def test_employee(self):
+  """Tests for the module employee."""
+
+  def setUp(self):
+    """Make an employee to use in tests."""
+    self.eric = Employee('eric', 'matthes', 65000)
+
+  def test_give_default_raise(self):
+    """Test that a default raise works correctly."""
+    self.eric.give_raise()
+    self.assertEqual(self.eric.salary, 70000)
+
+  def test_give_custom_raise(self):
+    """Test that a custom raise works correctly."""
+    self.eric.give_raise(10000)
+    self.assertEqual(self.eric.salary, 75000)
+
+    unittest.main()
