eafc6e1c3e0feb4e7fdd2f2685a9e291ac21b812
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2023-05-13 15:40:24 -0500

    Create ship.py

---
+++ Chapter 12 Exercises/ship.py
@@ -0,0 +1,38 @@
+import pygame
+
+class Ship:
+  """A class to manage the ship."""
+
+  def __init__(self, ai_game): #ai_game is a reference to the AlienInvasion class
+    """Initialize the ship and set its starting position."""
+    self.screen = ai_game.screen
+    self.screen_rect = ai_game.screen.get_rect()
+    self.settings = ai_game.settings
+
+    #Load the ship image and get its rect.
+    self.image = pygame.image.load('images/ship.bmp')
+    self.rect = self.image.get_rect()
+
+    #Start each new ship at the bottom center of the screen
+    self.rect.midbottom = self.screen_rect.midbottom
+    
+    #Store a float for the ship's exact horizontal position
+    self.x = float(self.rect.x)
+    #Movement flags; start with a ship that not moving
+    self.moving_right = False
+    self.moving_left = False
+
+  def update(self):
+    """Update the ship's position based on the movement flag."""
+    #Update the ship's x value, not the rect
+    if self.moving_right and self.rect.right < self.screen_rect.right:
+      self.x += self.settings.ship_speed
+    if self.moving_left and self.rect.left > 0:
+      self.x -= self.settings.ship_speed
+
+    #Update rect object from self.x
+    self.rect.x = self.x
+      
+  def blitme(self):
+    """Draw the ship at its current location."""
+    self.screen.blit(self.image, self.rect)
