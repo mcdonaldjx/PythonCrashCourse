c9b21c06bcda80f8e516f85afe9499c0997e9f71
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2023-05-13 20:20:42 -0500

    Update settings.py

--- Chapter 12 Exercises/settings.py
+++ Chapter 12 Exercises/settings.py
@@ -9,4 +9,11 @@ class Settings:
     self.bg_color = (230, 230, 230)
 
     #Ship settings
-    self.ship_speed = 6.0
+    self.ship_speed = 2.0
+
+    #Bullet settings
+    self.bullet_speed = 4.0
+    self.bullet_width = 3
+    self.bullet_height = 15
+    self.bullet_color = (60, 60, 60)
+    self.bullets_allowed = 3
