ccbdcfda4ad38a8928bcd631a310217cd4c89bf9
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2023-05-17 18:17:20 -0500

    Create raindrop.py

---
+++ Chapter 13 Exercises/Exercise 13-4/raindrop.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Raindrop(Sprite):
+
+  def __init__(self, game):
+    super().__init__()
+    self.image = pygame.image.load('images/raindrop.bmp')
+    self.screen = game.screen
+    self.rect = self.image.get_rect()
+    self.speed = float(1.1)
+
+  def update(self):
+    self.y += self.speed
+    self.rect.y = self.y
+    
+    
