42d499cdfe5d78fb0900e0ab487919c4a83d8af5
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2023-05-30 19:51:29 -0500

    Update and rename main.py to press_p_to_play.py

--- Chapter 14 Exercises/Exercise 14-1/press_p_to_play.py
+++ Chapter 14 Exercises/Exercise 14-1/press_p_to_play.py
@@ -1,3 +1,5 @@
+# Created 05/30/2023 by Jared
+# 14-1. Press P to Play: Because Alien Invasion uses keyboard input to control the ship, it would be useful to start the game with a keypress. Add code that lets the player press P to start. It might help to move some code from _check_play_button() to a _start_game() method that can be called from _check_play_button() and _check_keydown_events().
 import sys
 from time import sleep
 
